b519060c010d12ba08927b87a0916a9ca1cf2307
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-06-24 09:37:20 +0430

    update, delete, get functions

--- connections/dbconnection.go
+++ connections/dbconnection.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"github.com/Mohammad-Hakemi22/mongoAPI/models"
 
+	"github.com/Mohammad-Hakemi22/mongoAPI/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -37,6 +39,47 @@ func insertOneMovie(movie models.Netflix) {
 	inserted, err := collection.InsertOne(context.Background(), movie)
 	checkError(err)
 	fmt.Println("inserted movie: OK; movieID: ", inserted.InsertedID)
+}
+
+func updateOneMovie(movieID string) {
+	id, err := primitive.ObjectIDFromHex(movieID)
+	checkError(err)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"whatched": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	checkError(err)
+	fmt.Println("updated one movie: OK; count: ",result.ModifiedCount)
+}
+
+func deleteOneMovie(movieID string) {
+	id, err := primitive.ObjectIDFromHex(movieID)
+	checkError(err)
+	filter := bson.M{"_id": id}
+	result, err := collection.DeleteOne(context.Background(), filter)
+	checkError(err)
+	fmt.Println("deleted one movie: OK; count: ", result.DeletedCount)
+}
+
+func deleteAllMovies() {
+	result, err := collection.DeleteMany(context.Background(), bson.D{{}})
+	checkError(err)
+	fmt.Println("deleted all movies: OK; count: ", result.DeletedCount)
+}
+
+func GetAllMovies_helper() []bson.M {
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	defer cursor.Close(context.Background())
+	checkError(err)
+	// var movies []primitive.M
+	var movies []bson.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+		checkError(err)
+		movies = append(movies, movie)
+	}
+	return movies
 }
 
 func checkError(err error) {

60a317651f7face4b86614dcb602f17d3db2e9d1
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-06-25 17:24:37 +0430

    start server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Mohammad-Hakemi22/mongoAPI/router"
+)
 
 func main() {
-	fmt.Println("mongo API")
+	fmt.Println("movies whatched list API")
+	fmt.Println("Starting server ...")
+	r := router.Router()
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
